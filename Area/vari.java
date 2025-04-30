3fd28281684e531dae1c209971d29ff27abf2089
chaitanya9160 <162734569+chaitanya9160@users.noreply.github.com>
2025-04-30 13:01:20 +0530

    Add files via upload

---
+++ Area/vari.java
@@ -0,0 +1,25 @@
+import java.io.*;
+class vari
+{
+public static void main(String args[])
+{
+byte var1=127;
+short var2=32333;
+int var3=1234567;
+long var4=80103107L;
+float var5=324.678f;
+double var6=124546.321;
+char var7='A';
+String var8="Chaitanya";
+boolean var9=true;
+System.out.println(var1);
+System.out.println(var2);
+System.out.println(var3);
+System.out.println(var4);
+System.out.println(var5);
+System.out.println(var6);
+System.out.println(var7);
+System.out.println(var8);
+System.out.println(var9);
+}
+}
