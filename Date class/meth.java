5ff96baabff84f1916dcacfec654c67456f45071
chaitanya9160 <162734569+chaitanya9160@users.noreply.github.com>
2025-05-04 23:08:47 +0530

    Add files via upload

---
+++ Date class/meth.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class meth
+{
+	public static void main(String arg[])
+	{
+		Date obj=new Date();
+		System.out.println(obj);
+		Date obj1=new Date(25000L);//java developing date o/p
+		System.out.println(obj1);
+		
+		
+	}
+}
