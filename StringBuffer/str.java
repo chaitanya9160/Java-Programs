973319e28a1d8af240c1443a2fb1aa80feeeb2ce
chaitanya9160 <162734569+chaitanya9160@users.noreply.github.com>
2025-05-06 23:03:42 +0530

    Add files via upload

---
+++ StringBuffer/str.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class str
+{
+	public static void main(String arg[])
+	{
+		String nm="Chaitu";
+		String obj = new String("Patil");
+		
+		System.out.println(nm);
+		System.out.println(obj);
+		
+		System.out.println(nm.length());
+		System.out.println(obj.length());
+	}
+}
---
+++ StringBuffer/strBuff.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+class strBuff
+{
+	public static void main(String arg[])
+	{
+		//StringBuffer nm="Chaitu";
+		StringBuffer obj = new StringBuffer("Patil");
+		
+		System.out.println(obj.appe);
+	}
+}
