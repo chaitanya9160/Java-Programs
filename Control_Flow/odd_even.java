e9d46ef82489081313658e2d26f322e7bac283a9
chaitanya9160 <162734569+chaitanya9160@users.noreply.github.com>
2025-05-15 22:50:33 +0530

    Add files via upload

---
+++ Control_Flow/odd_even.java
@@ -0,0 +1,11 @@
+class EvenOddCheck {
+    public static void main(String[] args) {
+        int num = 7;
+
+        if (num % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+    }
+}
